c86b406446377f34ccf95e80c2e31342143a6ce5
MythicalFlame <82922845+MythicalFlame@users.noreply.github.com>
2021-09-07 22:35:37 -0500

    Update index.js
    Add `discord` command

--- src/BP_GlobalSphere/scripts/index.js
+++ src/BP_GlobalSphere/scripts/index.js
@@ -8,11 +8,13 @@ function customCommand(command, msg) {
     if (command.split(" ")[0] == "spawn") {
         Commands.run(`execute "${msg.sender.name}" ~~~ tp 1399 67 1656`)
     } else if (command.split(" ")[0] == "help") {
-        Commands.run(`tellraw "${msg.sender.name}" {"rawtext":[{"text":"§7- ${commandPrefix}help: §aLists all custom commands available.\\n§7- ${commandPrefix}spawn: Teleports the player to spawn/hub.\\n§7- ${commandPrefix}version: §aShows the Behaviour Pack version.\\n§7- ${commandPrefix}credits: §aLists credits."}]}`)
+        Commands.run(`tellraw "${msg.sender.name}" {"rawtext":[{"text":"§7- ${commandPrefix}help: §aLists all custom commands available.\\n§7- ${commandPrefix}spawn: §aTeleports the player to spawn/hub.\\n§7- ${commandPrefix}discord: §aShows the discord link.\\n§7- ${commandPrefix}version: §aShows the Behaviour Pack version.\\n§7- ${commandPrefix}credits: §aLists credits."}]}`)
     } else if (command.split(" ")[0] == "credits") {
         Commands.run(`tellraw "${msg.sender.name}" {"rawtext":[{"text":"Resource Pack created by LeRabbitKing. Custom Command Library created by Sammster 10 on Youtube."}]}`)
     } else if (command.split(" ")[0] == "version") {
         Commands.run(`tellraw "${msg.sender.name}" {"rawtext":[{"text":"BP Version: 1.2.0"}]}`)
+    } else if (command.split(" ")[0] == "discord") {
+        Commands.run(`tellraw "${msg.sender.name}" {"rawtext":[{"text":"Discord Link: https://discord.gg/d4hCeZA8"}]}`)
     } else {
         Commands.run(`tellraw "${msg.sender.name}" {"rawtext":[{"text":"§cCustom command not found! Please type §7${commandPrefix}help §cfor a list of all avalible commands"}]}`)
     }
